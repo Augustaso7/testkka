e80431ae44f6500e8f9241adee82951a7f6a5c70
Augustaso7 <augusto_cabalin@hotmail.com>
2024-05-08 13:14:40 -0300

    obtencion de datos de la api y presentacion de los mismos en html

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+// Si uso ngOnInit, es buena practica agregar la interfaz OnInit en el export para asegurar que el metodo se ejecute correctamente
+export class Tab3Page implements OnInit {
+  catFacts: any;
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.obtenerDatos().subscribe({
+      next: (data) => {
+        this.catFacts = data;
+      },
+      error: (error) => {
+        console.error('Error al obtener los datos:', error);
+      }
+    });
+  }
 
 }
